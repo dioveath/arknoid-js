3a7be6b32f6989a962dd3b98165f36654cf86bda
charicha <raisaroj360@gmail.com>
2017-09-26 10:10:20 +0545

    I have commited this

---
+++ main.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+
+  var canvas = document.getElementById("canvas"),
+  context = canvas.getContext("2d"),
+  width = canvas.width = window.innerWidth,
+  height = canvas.height = window.innerHeight;
+
+  var ball = particle.create(0, 0, width, height);
+
+};
